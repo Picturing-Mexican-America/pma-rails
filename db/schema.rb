14fdf1e240e73f2d9f136af08c8e4366cc274831
Tim Urian <tim.urian@gmail.com>
2020-08-04 08:00:47 -0700

    Fix name of User email column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_152317) do
+ActiveRecord::Schema.define(version: 2020_08_04_144438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "mail"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
