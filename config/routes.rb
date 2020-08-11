edee74913427e5f54d2f49653e545a9d2afa2646
Tim Urian <tim.urian@gmail.com>
2020-08-11 08:21:19 -0700

    Create, edit, show actions for Place

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :places, only: [:index]
+  resources :places
 end
