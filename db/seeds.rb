b892ba11fe51c742ad857436d90ec020eeb21850
Tim Urian <tim.urian@gmail.com>
2020-10-05 08:09:47 -0700

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!([
+  {
+    en_name: 'En-Frogtown Brewery',
+    es_name: 'Es-Frogtown Brewery',
+    en_description: 'en-frogtown brewery description',
+    es_description: 'es-frogtown brewery description',
+    category: 'brewery',
+    lat: 34.107025,
+    lon: -118.25136
+  },
+  {
+    en_name: 'En-Golden Road Brewery',
+    es_name: 'En-Golden Road Brewery',
+    en_description: 'en-golden road brewery description',
+    es_description: 'es-golden road brewery description',
+    category: 'brewery',
+    lat: 34.150672,
+    lon: -118.274452
+  },
+  {
+    en_name: 'En-Mohawk Bend',
+    es_name: 'Es-Mohawk Bend',
+    en_description: 'en-Mohawk Bend description',
+    es_description: 'es-Mohawk Bend description',
+    category: 'restaurant',
+    lat: 34.077695 ,
+    lon: -118.264768
+  }
+])
+
+# Template
+# en_name:
+# es_name:
+# en_description:
+# es_description:
+# category:
+# lat:
+# lon:
