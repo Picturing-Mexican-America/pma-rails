0c13ad20ee48e81842ad762dbf369c6894a93b79
Lexi Quint <lexiqt@gmail.com>
2021-03-23 15:44:26 -0700

    update place serializer

--- app/serializers/api/v1/place_serializer.rb
+++ app/serializers/api/v1/place_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::PlaceSerializer < ActiveModel::Serializer
-  attributes :id, :en_name, :es_name, :en_description, :es_description, :category, :lat, :lon
+  attributes :id, :en_name, :es_name, :en_description, :es_description, :category, :lat, :lon, :remote_url
 end
